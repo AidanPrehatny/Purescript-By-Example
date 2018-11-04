79bf2e36415fb44f20f84035f4fe8497134b805d
Aidan <aprehatny@yahoo.com>
2018-11-04 11:59:12 -0800

    Being Address book

--- exercises/chapter3/src/Data/AddressBook.purs
+++ exercises/chapter3/src/Data/AddressBook.purs
@@ -6,35 +6,16 @@ import Control.Plus (empty)
 import Data.List (List(..), filter, head)
 import Data.Maybe (Maybe)
 
-type Address =
-  { street :: String
-  , city   :: String
-  , state  :: String
-  }
+type Address = 
+    {street :: String
+    , city :: String
+    , state :: String
+    }
 
 type Entry =
-  { firstName :: String
-  , lastName  :: String
-  , address   :: Address
-  }
+    { firstName :: String
+    , lastName :: String
+    , address :: Address
+    }
 
 type AddressBook = List Entry
-
-showAddress :: Address -> String
-showAddress addr = addr.street <> ", " <> addr.city <> ", " <> addr.state
-
-showEntry :: Entry -> String
-showEntry entry = entry.lastName <> ", " <> entry.firstName <> ": " <> showAddress entry.address
-
-emptyBook :: AddressBook
-emptyBook = empty
-
-insertEntry :: Entry -> AddressBook -> AddressBook
-insertEntry = Cons
-
-findEntry :: String -> String -> AddressBook -> Maybe Entry
-findEntry firstName lastName = head <<< filter filterEntry
-  where
-  filterEntry :: Entry -> Boolean
-  filterEntry entry = entry.firstName == firstName && entry.lastName == lastName
-
