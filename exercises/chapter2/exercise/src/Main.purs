6749063d85ecf9c8a9fdae58e9f836a6d0f79ca8
Aidan <aprehatny@yahoo.com>
2018-11-04 11:36:27 -0800

    Defined diagonal and circle of area funcs

--- exercises/chapter2/exercise/src/Main.purs
+++ exercises/chapter2/exercise/src/Main.purs
@@ -1,5 +1,15 @@
 module Main where
-  
-  import Effect.Console
 
-  main = log "Hello, World!"
+import Prelude
+import Effect (Effect)
+import Effect.Console (logShow)
+import Math (sqrt, pi)
+
+diagonal :: Number -> Number -> Number
+diagonal w h = sqrt (w * w + h * h)
+
+circleArea :: Number -> Number
+circleArea r = pi * (r * r)
+
+main :: Effect Unit
+main = logShow (circleArea 3.0)
